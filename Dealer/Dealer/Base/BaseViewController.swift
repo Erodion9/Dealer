5a9e2de5c5ba77e89e4ea2862751f6fed55c5d22
Deniz Mavi <denizmavi@sabanciuniv.edu>
2020-01-26 01:25:04 +0300

    Add Base View Controller and Model

---
+++ Dealer/Dealer/Base/BaseViewController.swift
@@ -0,0 +1,11 @@
+//
+//  BaseViewController.swift
+//  Dealer
+//
+//  Created by Deniz Mavi on 26.01.2020.
+//  Copyright © 2020 Deniz Mavi. All rights reserved.
+//
+
+import UIKit
+
+class BaseViewController: UIViewController { }
---
+++ Dealer/Dealer/Base/BaseViewModel.swift
@@ -0,0 +1,9 @@
+//
+//  BaseViewModel.swift
+//  Dealer
+//
+//  Created by Deniz Mavi on 26.01.2020.
+//  Copyright © 2020 Deniz Mavi. All rights reserved.
+//
+
+class BaseViewModel { }
