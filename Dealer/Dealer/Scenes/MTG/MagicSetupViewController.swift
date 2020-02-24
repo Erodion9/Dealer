416884bd2f06b74a9ee2822749f9fbc586bf9143
Deniz Mavi <denizmavi@sabanciuniv.edu>
2020-02-24 10:07:16 +0300

    Add MagicSetupView UI Elements

--- Dealer/Dealer/Scenes/MTG/MagicSetupViewController.swift
+++ Dealer/Dealer/Scenes/MTG/MagicSetupViewController.swift
@@ -10,4 +10,29 @@ import UIKit
 
 final class MagicSetupViewController: BaseViewController {
     
+    @IBOutlet private weak var startButton: UIButton!
+    @IBOutlet private weak var roundsSegControl: UISegmentedControl!
+    @IBOutlet private weak var player1ColorTextField: UITextField!
+    @IBOutlet private weak var player1NameTextField: UITextField!
+    @IBOutlet private weak var player1StartHPTextField: UITextField!
+    @IBOutlet private weak var player2ColorTextField: UITextField!
+    @IBOutlet private weak var player2NameTextField: UITextField!
+    @IBOutlet private weak var player2StartHPTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func startButtonTapped(_ sender: Any) {
+        show(storyboard: .magic, style: .overFullScreen, passedParameters: nil)
+    }
+    
 }
+
+extension MagicSetupViewController: Routable {
+    
+    enum StoryboardIdentifier: String {
+        case magic = "Magic"
+    }
+}
+
