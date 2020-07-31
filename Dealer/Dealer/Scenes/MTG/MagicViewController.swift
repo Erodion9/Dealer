41613a2c52b98793af8e829bb0048463c0eab666
Erodion9 <denizmavi@sabanciuniv.edu>
2020-07-31 22:30:01 +0300

    Add MTGView storyboard layout

--- Dealer/Dealer/Scenes/MTG/MagicViewController.swift
+++ Dealer/Dealer/Scenes/MTG/MagicViewController.swift
@@ -5,12 +5,24 @@
 //  Created by Deniz Mavi on 17.02.2020.
 //  Copyright © 2020 Deniz Mavi. All rights reserved.
 //
-
 import UIKit
 
 final class MagicViewController: BaseViewController {
     
+    @IBOutlet private weak var player2View: UIView!
+    @IBOutlet private weak var player1ToolbarView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        configureView()
+    }
+}
+
+//MARK: - View Configuration
+extension MagicViewController {
+    
+    private func configureView() {
+        player2View.transform = CGAffineTransform(rotationAngle: CGFloat.pi)
+        player1ToolbarView.transform = CGAffineTransform(rotationAngle: CGFloat.pi)
     }
 }
