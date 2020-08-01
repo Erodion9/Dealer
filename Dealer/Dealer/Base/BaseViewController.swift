1c0ad68018b390e1987ccefb43f179c8bfdbd8ad
Erodion9 <denizmavi@sabanciuniv.edu>
2020-08-01 19:55:19 +0300

    Add 'push view up when keyboard appears' functionality

--- Dealer/Dealer/Base/BaseViewController.swift
+++ Dealer/Dealer/Base/BaseViewController.swift
@@ -12,6 +12,26 @@ class BaseViewController: UIViewController {
     
     var passedParameters: Any?
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow), name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide), name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+
+    @objc func keyboardWillShow(notification: NSNotification) {
+        if let keyboardSize = (notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue {
+            if self.view.frame.origin.y == 0 {
+                self.view.frame.origin.y -= keyboardSize.height
+            }
+        }
+    }
+
+    @objc func keyboardWillHide(notification: NSNotification) {
+        if self.view.frame.origin.y != 0 {
+            self.view.frame.origin.y = 0
+        }
+    }
+    
     func resizeImage(image: UIImage, targetSize: CGSize) -> UIImage {
         let size = image.size
 
